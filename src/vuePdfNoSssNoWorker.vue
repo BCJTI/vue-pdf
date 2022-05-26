a079504362a920aa30de9941f1e3ce0d1e8a24be
Felipe Lopes <carcamano@gmail.com>
2022-05-25 21:33:49 -0300

    Fix: Global Worker

--- src/vuePdfNoSssNoWorker.vue
+++ src/vuePdfNoSssNoWorker.vue
@@ -4,12 +4,12 @@
 	import componentFactory from './componentFactory.js'
 
 	if ( process.env.VUE_ENV !== 'server' ) {
-
 		var pdfjsWrapper = require('./pdfjsWrapper.js').default;
+		// var PDFJS = require('pdfjs-dist/es5/build/pdf.js');
 		var PDFJS = require('pdfjs-dist/es5/build/pdf.js');
+		PDFJS.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 		var component = componentFactory(pdfjsWrapper(PDFJS));
 	} else {
-
 		var component = componentFactory({});
 	}
 
