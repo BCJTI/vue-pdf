dca4849b27d2b85a4dad47feccd637b7990d730d
Felipe Lopes <carcamano@gmail.com>
2022-05-26 11:22:59 -0300

    Impl: Vue PDF Global Worker

--- src/vuePdfNoSssGlobalWorker.vue
+++ src/vuePdfNoSssGlobalWorker.vue
@@ -4,10 +4,20 @@
 	import componentFactory from './componentFactory.js'
 
 	if ( process.env.VUE_ENV !== 'server' ) {
+
 		var pdfjsWrapper = require('./pdfjsWrapper.js').default;
 		var PDFJS = require('pdfjs-dist/es5/build/pdf.js');
+		PDFJS.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${PDFJS.version}/pdf.worker.js`;
+
+		if ( typeof window !== 'undefined' && 'Worker' in window && navigator.appVersion.indexOf('MSIE 10') === -1 ) {
+
+			var PdfjsWorker = require('worker-loader!pdfjs-dist/es5/build/pdf.worker.js');
+			PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
+		}
+
 		var component = componentFactory(pdfjsWrapper(PDFJS));
 	} else {
+
 		var component = componentFactory({});
 	}
 
